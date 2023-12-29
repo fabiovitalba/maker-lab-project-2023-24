aac76b3fa41acf87854ecbab085399a7753ca28c
Fabio.Vitalba <fabvit9@gmail.com>
2023-12-29 20:30:41 +0100

    Added function to read items from adafruit

--- src/adafruit_connector.py
+++ src/adafruit_connector.py
@@ -1,9 +1,39 @@
 import time
 from Adafruit_IO import Client, Data, Feed, RequestError
 from env import adafruit_user, adafruit_key
+import pandas as pd
 
-# create an instance of the REST client.
-aio = Client(adafruit_user, adafruit_key)
+items_structure = {
+    'Timestamp': [],
+    'Barcode': [],
+    'Description': [],
+    'Expiration Date': [],
+    'Quantity': [],
+    'Date added': [],
+    'Date modified': []
+}
+
+def read_items(key):
+    # create an instance of the REST client.
+    aio = Client(adafruit_user, adafruit_key)
+
+    # Access existing or create new FEED using the name/key passed as parameter
+    try:
+        # Open Adafruit Feed, retrieve its data (which is in JSON format), create DataFrame from the JSON
+        feed = aio.feeds(key)
+        feed_data = aio.receive(feed.key)
+        items = pd.read_json(feed_data.value)
+    except RequestError:
+        # If an error is thrown, we assume the Adafruit Feed does not exist
+        # In this case we create a new Adafruit Feed, a new DataFrame using the predefined Structure, and store the new DataFrame in Adafruit
+        feed = aio.create_feed(Feed(name=key, key=key, history=False))
+        items = pd.DataFrame(items_structure)
+        items_json = items.to_json(orient='records')
+        aio.send_data(feed.key, items_json)
+    
+    return items
+    
+    
 
 # Adafruit connector taken from lab exercises
 """
