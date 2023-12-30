42d4b8d7799cc74ba88f0a682a7be9c8cf9ae0d2
Fabio.Vitalba <fabvit9@gmail.com>
2023-12-30 18:32:23 +0100

    Fixed StringIO JSON-Literal warning

--- src/adafruit_connector.py
+++ src/adafruit_connector.py
@@ -1,4 +1,5 @@
-from Adafruit_IO import Client, Data, Feed, RequestError
+from Adafruit_IO import Client, Feed, RequestError
+from io import StringIO
 from env import adafruit_user, adafruit_key
 import pandas as pd
 
@@ -21,7 +22,7 @@ def read_items(key):
         # Open Adafruit Feed, retrieve its data (which is in JSON format), create DataFrame from the JSON
         feed = aio.feeds(key)
         feed_data = aio.receive(feed.key)
-        items = pd.read_json(feed_data.value)
+        items = pd.read_json(StringIO(feed_data.value))
     except RequestError:
         # If an error is thrown, we assume the Adafruit Feed does not exist
         # In this case we create a new Adafruit Feed, a new DataFrame using the predefined Structure, and store the new DataFrame in Adafruit
@@ -36,6 +37,7 @@ def add_item(key, items_df, item):
     # create an instance of the REST client.
     aio = Client(adafruit_user, adafruit_key)
 
+    # add item to the adafruit item table
     try:
         feed = aio.feeds(key)
 
@@ -45,41 +47,3 @@ def add_item(key, items_df, item):
         return True
     except RequestError:
         return False
-    
-
-# Adafruit connector taken from lab exercises
-"""
-
-
-#----ACCESS OR CREATE FEED
-# access or create a feed with a given key or name
-try: # access the feed with key 'button' if it exists
-    feed = aio.feeds('button')
-except RequestError: # create a 'button' (name, key) feed otherwise
-    feed = aio.create_feed(Feed(name='button', key='button', history=False))
-
-
-#----SEND/POST DATA TO FEED
-# define a function that sends the latest data for a feed identified by key
-def button_status_send_data(key, data):
-    return aio.send_data(key, data)
-
-# add the data value of the Button status to the feed, identified by its key
-
-button_status = 0
-try:
-    while True:
-        # button_status = int(eh.input.one.read())
-        ## conditional expression 
-        ## uncomment for button managed via gpiozero/explorerhat
-        # button_status = 1 if button.is_pressed else 0
-        button_status = 1 if randrange(0,2) == 1 else 0
-        button_status_send_data(feed.key, button_status)
-        time.sleep(2)
-except KeyboardInterrupt:
-    print('bye')
-finally:
-    print('game over')
-
-
-"""
--- src/adafruit_connector_test.py
+++ src/adafruit_connector_test.py
@@ -4,16 +4,16 @@ from datetime import datetime
 
 # In order to retrieve the existing items from adafruit, we need to call the read_items method from adafruit
 items = read_items(ITEMS_KEY)
-print(items)
 
 # In order to add a new Item to the set, we need to initialize a List (Array) with the new data and call the add_item() method.
+# The List is in format [timestamp, barcode, description, expiration_date, quantity, datetime_created, datetime_modified]
 new_item = [datetime.now(), 'barcode', 'description', 'expiration date', 1, datetime.now(), None]
-print(new_item)
-add_item(ITEMS_KEY,items,new_item)
+if not add_item(ITEMS_KEY,items,new_item):
+    print('\033[91mADD_ITEM-method FAILED\033[0m')
 
 # Read items from Adafruit again in order to compare the cloud data with the local data
 items_updated = read_items(ITEMS_KEY)
-print('\n\n\n##### ADAFRUIT #####')
+print('##### ADAFRUIT #####')
 print(items_updated)
 print('\n##### LOCAL ####')
 print(items)
