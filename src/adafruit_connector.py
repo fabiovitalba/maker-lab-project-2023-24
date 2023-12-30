19421f1213a66cc9cf1e9b719a3324c52c916820
Fabio.Vitalba <fabvit9@gmail.com>
2023-12-30 19:11:47 +0100

    Implemented method to retrieve product name from barcode

--- src/adafruit_connector.py
+++ src/adafruit_connector.py
@@ -1,9 +1,10 @@
 from Adafruit_IO import Client, Feed, RequestError
 from io import StringIO
-from const import ITEMS_KEY, BARCODE_DICT_KEY
+from const import ITEMS_KEY
 from env import adafruit_user, adafruit_key
 import pandas as pd
 
+# region Items-methods
 items_structure = {
     'Timestamp': [],
     'Barcode': [],
@@ -14,12 +15,6 @@ items_structure = {
     'Date modified': []
 }
 
-barcode_dict_structure = {
-    'Barcode': [],
-    'Description': []
-}
-
-# region Items-methods
 def read_items():
     # create an instance of the REST client.
     aio = Client(adafruit_user, adafruit_key)
@@ -62,31 +57,3 @@ def reduce_item(items_df, barcode, quantity_diff):
 def remove_item(items_df, barcode):
     return False
 # endregion Items-methods
-
-# region Barcode-methods
-def read_barcode_dict():
-    # create an instance of the REST client.
-    aio = Client(adafruit_user, adafruit_key)
-
-    # Access existing or create new FEED using the name/key passed as parameter
-    try:
-        # Open Adafruit Feed, retrieve its data (which is in JSON format), create DataFrame from the JSON
-        feed = aio.feeds(BARCODE_DICT_KEY)
-        feed_data = aio.receive(feed.key)
-        barcode_dict = pd.read_json(StringIO(feed_data.value))
-    except RequestError:
-        # If an error is thrown, we assume the Adafruit Feed does not exist
-        # In this case we create a new Adafruit Feed, a new DataFrame using the predefined Structure, and store the new DataFrame in Adafruit
-        feed = aio.create_feed(Feed(name=BARCODE_DICT_KEY, key=BARCODE_DICT_KEY, history=False))
-        barcode_dict = pd.DataFrame(items_structure)
-        barcode_dict_json = barcode_dict.to_json(orient='records')
-        aio.send_data(feed.key, barcode_dict_json)
-    
-    return barcode_dict
-
-def add_barcode_dict(barcode, description):
-    return False
-
-def remove_barcode_dict(barcode):
-    return False
-# endregion Barcode-methods
--- src/const.py
+++ src/const.py
@@ -1,2 +1,1 @@
 ITEMS_KEY="items"
-BARCODE_DICT_KEY="barcode_dict"
