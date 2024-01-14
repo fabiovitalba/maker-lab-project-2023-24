80514684ff32801a2032827ce24892a97f5afd9c
Fabio.Vitalba <fabvit9@gmail.com>
2024-01-15 00:08:28 +0100

    Added change events to add item window

--- src/add_item_window.py
+++ src/add_item_window.py
@@ -14,6 +14,18 @@ def add_item_window(win_height, win_width, button_font, items_df):
     input_width = 450
     input_height = 100
 
+    def on_barcode_change(event):
+        print(barcode_entry.get())
+
+    def on_description_change(event):
+        print(description_entry.get())
+
+    def on_quantity_change(event):
+        print(quantity_entry.get())
+
+    def on_exp_date_change(event):
+        print(expiration_date_entry.get())
+
     def confirm_input():
         barcode_value = barcode_entry.get()
         description_value = description_entry.get()
@@ -43,6 +55,7 @@ def add_item_window(win_height, win_width, button_font, items_df):
     barcode_label = ctk.CTkLabel(barcode_frame, text="Barcode:", width=label_width, height=label_height, font=label_font)
     barcode_label.pack(side=ctk.LEFT, padx=10)
     barcode_entry = ctk.CTkEntry(barcode_frame, width=input_width, height=input_height, font=input_font)
+    barcode_entry.bind("<Key>", on_barcode_change)
     barcode_entry.pack(side=ctk.LEFT)
 
     description_frame = ctk.CTkFrame(ai_window)
@@ -50,6 +63,7 @@ def add_item_window(win_height, win_width, button_font, items_df):
     description_label = ctk.CTkLabel(description_frame, text="Description:", width=label_width, height=label_height, font=label_font)
     description_label.pack(side=ctk.LEFT, padx=10)
     description_entry = ctk.CTkEntry(description_frame, width=input_width, height=input_height, font=input_font)
+    description_entry.bind("<Key>", on_description_change)
     description_entry.pack()
 
     quantity_frame = ctk.CTkFrame(ai_window)
@@ -57,6 +71,7 @@ def add_item_window(win_height, win_width, button_font, items_df):
     quantity_label = ctk.CTkLabel(quantity_frame, text="Quantity:", width=label_width, height=label_height, font=label_font)
     quantity_label.pack(side=ctk.LEFT, padx=10)
     quantity_entry = ctk.CTkEntry(quantity_frame, width=input_width, height=input_height, font=input_font)
+    quantity_entry.bind("<Key>", on_quantity_change)
     quantity_entry.pack(side=ctk.LEFT)
 
     expiration_date_frame = ctk.CTkFrame(ai_window)
@@ -64,6 +79,7 @@ def add_item_window(win_height, win_width, button_font, items_df):
     expiration_date_label = ctk.CTkLabel(expiration_date_frame, text="Expiration Date:", width=label_width, height=label_height, font=label_font)
     expiration_date_label.pack(side=ctk.LEFT, padx=10)
     expiration_date_entry = ctk.CTkEntry(expiration_date_frame, width=input_width, height=input_height, font=input_font)
+    expiration_date_entry.bind("<Key>", on_exp_date_change)
     expiration_date_entry.pack(side=ctk.LEFT)
 
     # Create Confirm and Cancel buttons side by side
