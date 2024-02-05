4bb4607bfc9dcf2b60b67dc4000b0bca8ed7d52f
Daniel Di Bella <daniel.lechner2@icloud.com>
2024-02-05 15:59:57 +0100

    added counter in add and  float to int in item_list

--- src/add_item_window.py
+++ src/add_item_window.py
@@ -6,6 +6,7 @@ from item import find_desc_from_barcode
 from adafruit_connector import add_item
 from expiration_date import handle_exp_date_input
 
+
 def add_item_window(win_height, win_width, button_font, items_df):
     ai_window = ctk.CTkToplevel()
     canvas = ctk.CTkCanvas(ai_window, height=win_height, width=win_width)
@@ -18,7 +19,8 @@ def add_item_window(win_height, win_width, button_font, items_df):
     input_font = ("Helvetica", 20, "bold")
     input_width = 600
     input_height = 100
-    curr_exp_date_list = [datetime.date.today()]    #This must be a list in order to be mutable from the change_event
+    # This must be a list in order to be mutable from the change_event
+    curr_exp_date_list = [datetime.date.today()]
 
     def on_barcode_change(event):
         barcode = barcode_entry.get()
@@ -30,37 +32,36 @@ def add_item_window(win_height, win_width, button_font, items_df):
             else:
                 # Set the description and jump to quantity
                 description_entry.delete(0, ctk.END)
-                description_entry.insert(0,description)
-                quantity_entry.delete(0, ctk.END)
-                quantity_entry.insert(0, str(1.0))
-                quantity_entry.focus()
+                description_entry.insert(0, description)
+                expiration_date_entry.delete(0, ctk.END)
+                expiration_date_entry.insert(
+                    0, str(curr_exp_date_list[0].strftime("%d/%m/%Y")))
+                expiration_date_entry.focus()
+                confirm_button.configure(state="normal")
 
     def on_description_change(event):
         if event.keysym == "Return":
             description = description_entry.get()
             if description != "":
-                quantity_entry.delete(0, ctk.END)
-                quantity_entry.insert(0, str(1.0))
-                quantity_entry.focus()
-
-    def on_quantity_change(event):
-        if event.keysym == "Return":
-            expiration_date_entry.delete(0, ctk.END)
-            expiration_date_entry.insert(0, str(curr_exp_date_list[0].strftime("%d/%m/%Y")))
-            expiration_date_entry.focus()
-            confirm_button.configure(state="normal")
+                expiration_date_entry.delete(0, ctk.END)
+                expiration_date_entry.insert(
+                    0, str(curr_exp_date_list[0].strftime("%d/%m/%Y")))
+                expiration_date_entry.focus()
+                confirm_button.configure(state="normal")
 
     def on_exp_date_change(event, exp_date_list):
         exp_date_text = expiration_date_entry.get()
         next_input = False
         if event.keysym == "Return":
-            [exp_date_list[0], next_input] = handle_exp_date_input(exp_date_text, exp_date_list[0])
+            [exp_date_list[0], next_input] = handle_exp_date_input(
+                exp_date_text, exp_date_list[0])
             if next_input:
                 confirm_input()
             else:
                 expiration_date_entry.delete(0, ctk.END)
-                expiration_date_entry.insert(0, str(exp_date_list[0].strftime("%d/%m/%Y")))
-                expiration_date_entry.focus()    
+                expiration_date_entry.insert(
+                    0, str(exp_date_list[0].strftime("%d/%m/%Y")))
+                expiration_date_entry.focus()
 
     def confirm_input():
         barcode_value = barcode_entry.get()
@@ -68,13 +69,13 @@ def add_item_window(win_height, win_width, button_font, items_df):
         quantity_value = quantity_entry.get()
 
         # You can process the input values as needed
-        new_item = [barcode_value, description_value, pd.to_datetime(curr_exp_date_list[0]).timestamp(), quantity_value, pd.to_datetime("now").timestamp()]
+        new_item = [barcode_value, description_value, pd.to_datetime(
+            curr_exp_date_list[0]).timestamp(), quantity_value, pd.to_datetime("now").timestamp()]
         add_item(items_df, new_item)
 
         # Clear the entries for the next input
         barcode_entry.delete(0, ctk.END)
         description_entry.delete(0, ctk.END)
-        quantity_entry.delete(0, ctk.END)
         expiration_date_entry.delete(0, ctk.END)
         barcode_entry.focus()
         confirm_button.configure(state="disabled")
@@ -85,45 +86,69 @@ def add_item_window(win_height, win_width, button_font, items_df):
     # Create and pack the input labels and entry widgets
     barcode_frame = ctk.CTkFrame(ai_window)
     barcode_frame.pack(pady=10)
-    barcode_label = ctk.CTkLabel(barcode_frame, text="Barcode:", width=label_width, height=label_height, font=label_font)
+    barcode_label = ctk.CTkLabel(
+        barcode_frame, text="Barcode:", width=label_width, height=label_height, font=label_font)
     barcode_label.pack(side=ctk.LEFT, padx=10)
-    barcode_entry = ctk.CTkEntry(barcode_frame, width=input_width, height=input_height, font=input_font)
+    barcode_entry = ctk.CTkEntry(
+        barcode_frame, width=input_width, height=input_height, font=input_font)
     barcode_entry.bind("<Key>", on_barcode_change)
     barcode_entry.focus()
     barcode_entry.pack(side=ctk.LEFT)
 
     description_frame = ctk.CTkFrame(ai_window)
     description_frame.pack(pady=10)
-    description_label = ctk.CTkLabel(description_frame, text="Description:", width=label_width, height=label_height, font=label_font)
+    description_label = ctk.CTkLabel(
+        description_frame, text="Description:", width=label_width, height=label_height, font=label_font)
     description_label.pack(side=ctk.LEFT, padx=10)
-    description_entry = ctk.CTkEntry(description_frame, width=input_width, height=input_height, font=input_font)
+    description_entry = ctk.CTkEntry(
+        description_frame, width=input_width, height=input_height, font=input_font)
     description_entry.bind("<Key>", on_description_change)
     description_entry.pack()
 
     quantity_frame = ctk.CTkFrame(ai_window)
     quantity_frame.pack(pady=10)
-    quantity_label = ctk.CTkLabel(quantity_frame, text="Quantity:", width=label_width, height=label_height, font=label_font)
-    quantity_label.pack(side=ctk.LEFT, padx=10)
-    quantity_entry = ctk.CTkEntry(quantity_frame, width=input_width, height=input_height, font=input_font)
-    quantity_entry.bind("<Key>", on_quantity_change)
-    quantity_entry.pack(side=ctk.LEFT)
+    quantity_label = ctk.CTkLabel(
+        quantity_frame, text="Quantity:", width=label_width, height=label_height, font=label_font)
+    quantity_label.pack(side=ctk.LEFT, padx=(0, 10))
+    quantity_entry = ctk.StringVar(value="1")
+    quantity_label_value = ctk.CTkLabel(
+        quantity_frame, textvariable=quantity_entry, width=label_width, height=label_height, font=input_font)
+    quantity_label_value.pack(side=ctk.LEFT, padx=10)
+
+    def update_quantity(value):
+        current_value = float(quantity_entry.get())
+        new_value = max(1, current_value + value)
+        quantity_entry.set(f"{new_value:.0f}")
+
+    plus_button = ctk.CTkButton(
+        quantity_frame, text="+", command=lambda: update_quantity(1.0), font=input_font, height=button_height)
+    plus_button.pack(side=ctk.LEFT)
+
+    minus_button = ctk.CTkButton(
+        quantity_frame, text="-", command=lambda: update_quantity(-1.0), font=input_font,  height=button_height)
+    minus_button.pack(side=ctk.LEFT, padx=(10, 0))
 
     expiration_date_frame = ctk.CTkFrame(ai_window)
     expiration_date_frame.pack(pady=10)
-    expiration_date_label = ctk.CTkLabel(expiration_date_frame, text="Expiration Date (dd/mm/yyyy):", width=label_width, height=label_height, font=label_font)
+    expiration_date_label = ctk.CTkLabel(
+        expiration_date_frame, text="Expiration Date (dd/mm/yyyy):", width=label_width, height=label_height, font=label_font)
     expiration_date_label.pack(side=ctk.LEFT, padx=10)
-    expiration_date_entry = ctk.CTkEntry(expiration_date_frame, width=input_width, height=input_height, font=input_font)
-    expiration_date_entry.bind("<Key>", lambda event: on_exp_date_change(event=event, exp_date_list=curr_exp_date_list))
+    expiration_date_entry = ctk.CTkEntry(
+        expiration_date_frame, width=input_width, height=input_height, font=input_font)
+    expiration_date_entry.bind("<Key>", lambda event: on_exp_date_change(
+        event=event, exp_date_list=curr_exp_date_list))
     expiration_date_entry.pack(side=ctk.LEFT)
 
     # Create Confirm and Cancel buttons side by side
     button_frame = ctk.CTkFrame(ai_window)
     button_frame.pack(pady=10)
 
-    cancel_button = ctk.CTkButton(button_frame, text="Cancel", command=cancel_input, width=button_width, height=button_height, font=button_font)
-    cancel_button.pack(side=ctk.LEFT, padx=10)
+    cancel_button = ctk.CTkButton(button_frame, text="Cancel", command=cancel_input,
+                                  width=button_width, height=button_height, font=button_font)
+    cancel_button.pack(side=ctk.LEFT, padx=(0, 10))
 
-    confirm_button = ctk.CTkButton(button_frame, text="Confirm", command=confirm_input, width=button_width, height=button_height, font=button_font)
+    confirm_button = ctk.CTkButton(button_frame, text="Confirm", command=confirm_input,
+                                   width=button_width, height=button_height, font=button_font)
     confirm_button.configure(state="disabled")
     confirm_button.pack(side=ctk.LEFT)
 
--- src/gui_app.py
+++ src/gui_app.py
@@ -10,12 +10,14 @@ WIN_HEIGHT = 600
 TITLE_FONT = ("Helvetica", 18, "bold")
 BUTTON_FONT = ("Helvetica", 28, "bold")
 
+
 def display_message(message):
     print(message)
-    #message_label = tk.Label(root, text=message)
-    #message_label.pack(pady=10)
+    # message_label = tk.Label(root, text=message)
+    # message_label.pack(pady=10)
     # Auto-remove the message after a few seconds
-    #root.after(3000, message_label.destroy)
+    # root.after(3000, message_label.destroy)
+
 
 def main_menu_window(mm_window, items_df):
     canvas = ctk.CTkCanvas(mm_window, height=WIN_HEIGHT, width=WIN_WIDTH)
@@ -27,15 +29,20 @@ def main_menu_window(mm_window, items_df):
     button_width = 900
     button_height = 100
 
-    list_button = ctk.CTkButton(mm_window, text="List all items", command=lambda: item_list_window(WIN_HEIGHT, WIN_WIDTH, BUTTON_FONT, items_df), width=button_width, height=button_height, font=BUTTON_FONT)
+    list_button = ctk.CTkButton(mm_window, text="List all items", command=lambda: item_list_window(
+        WIN_HEIGHT, WIN_WIDTH, BUTTON_FONT, items_df), width=button_width, height=button_height, font=BUTTON_FONT)
     list_button.pack(pady=10)
-    add_button = ctk.CTkButton(mm_window, text="Add an item", command=lambda: add_item_window(WIN_HEIGHT, WIN_WIDTH, BUTTON_FONT, items_df), width=button_width, height=button_height, font=BUTTON_FONT)
+    add_button = ctk.CTkButton(mm_window, text="Add an item", command=lambda: add_item_window(
+        WIN_HEIGHT, WIN_WIDTH, BUTTON_FONT, items_df), width=button_width, height=button_height, font=BUTTON_FONT)
     add_button.pack(pady=10)
-    remove_button = ctk.CTkButton(mm_window, text="Remove an item", command=lambda: remove_item_window(WIN_HEIGHT, WIN_WIDTH, BUTTON_FONT, items_df), width=button_width, height=button_height, font=BUTTON_FONT)
+    remove_button = ctk.CTkButton(mm_window, text="Remove an item", command=lambda: remove_item_window(
+        WIN_HEIGHT, WIN_WIDTH, BUTTON_FONT, items_df), width=button_width, height=button_height, font=BUTTON_FONT)
     remove_button.pack(pady=10)
-    quit_button = ctk.CTkButton(mm_window, text="Quit", command=mm_window.destroy, width=button_width, height=button_height, font=BUTTON_FONT)
+    quit_button = ctk.CTkButton(mm_window, text="Quit", command=mm_window.destroy,
+                                width=button_width, height=button_height, font=BUTTON_FONT)
     quit_button.pack(pady=10)
     canvas.pack()
+
 
 def main():
     ctk.set_appearance_mode("dark")
@@ -47,6 +54,7 @@ def main():
     items_df = read_items()
     main_menu_window(window, items_df)
     window.mainloop()
+
 
 if __name__ == "__main__":
     main()
